ed5d909df0f5239053e3494d145f1ba5c19ec84f
dbwc <d.benseman@wc.school.nz>
2021-03-26 11:58:11 +1300

    Update with code to add signup

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, session, redirect
 import sqlite3
 from sqlite3 import Error
 
@@ -42,8 +42,32 @@ def render_menu():
 def render_login():
     return render_template('login.html')
 
-@app.route('/signup')
+@app.route('/signup', methods=['GET', 'POST'])
 def render_signup():
+    if request.method == 'POST':
+        fname = request.form.get('fname')
+        lname = request.form.get('lname')
+        email = request.form.get('email')
+        password = request.form.get('password')
+        password2 = request.form.get('password2')
+
+        if password2 != password:
+            return redirect('/signup?error=Passwords+do+not+match.')
+
+        if len(password) < 8:
+            return redirect('/signup?error=Password+must+be+at+least+8+characters+long.')
+
+        # Create the connection
+        con = create_connection(DB_NAME)
+
+        # Run the query
+        query = "INSERT INTO customers (id, fname, lname, email, password) VALUES (null, ?, ?, ?, ?)"
+
+        cur = con.cursor()
+        cur.execute(query, (fname, lname, email, password))
+        con.commit()
+        con.close()
+
     return render_template('signup.html')
 
 if __name__ == '__main__':
